8701b8b95e39224952d01e91fac42213c821c1ce
CharlotteFTD <liuyghit@163.com>
2024-09-25 14:27:02 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+# install cert-manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
+
+# install opentelemetry-operator
+kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
